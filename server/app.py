56c25a0a8d2289026294b847dd0c3dfce622ed87
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-09-20 15:17:41 -0400

    create models and routes

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db # import your models here!
+from models import db, Author, Book, Publisher 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -20,6 +20,45 @@ def index():
     return "Hello world"
 
 # write your routes here!
+@app.get('/authors/<int:id>')
+def author_by_id(id):
+    try:
+        author = Author.query.filter(Author.id == id).first()
+        return jsonify(author.to_dict()), 200
+    except:
+        return jsonify({'error': 'Author not found'}), 404
+    
+@app.get('/books')
+def all_books():
+    books = Book.query.all()
+    response = [book.to_dict() for book in books]
+    return jsonify(response), 200
 
+
+@app.post("/books")
+def create_book():
+    try:
+        data = request.json
+        new_book = Book(**data)
+        db.session.add(new_book)
+        db.session.commit()
+        return jsonify(new_book.to_dict()), 201
+    except:
+        return jsonify({'error': 'Invalid data'}), 406
+    
+@app.delete('/author/<int:id>')
+def delete_author(id):
+    try:
+        author = Author.query.filter(Author.id == id).first()
+
+        for book in author.books:
+            db.session.delete(book)
+
+        db.session.delete(author)
+        db.session.commit()
+        return {}, 204
+    except:
+        return jsonify( {'error':'Author not found'} ), 404
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
